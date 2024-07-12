0f58846184aa52fd6f82002c47065e944269d3e3
stnokott <30934296+stnokott@users.noreply.github.com>
2024-07-12 15:44:42 +0200

    chore(dev): merge script and server

---
+++ scripts/server/Rpc.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+
+using Api;
+
+namespace Rpc;
+
+public partial class Rpc : Node
+{
+	const int PORT = 55555;
+
+	private readonly Api.Client client = new();
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		var mpPeer = new ENetMultiplayerPeer();
+		mpPeer.CreateServer(PORT);
+		Multiplayer.PeerConnected += OnClientConnected;
+		Multiplayer.MultiplayerPeer = mpPeer;
+		GD.Print("server started on port " + PORT);
+	}
+
+	private async void OnClientConnected(long id)
+	{
+		GD.Print("client connected: " + id);
+
+		//var serverStatus = await client.GetServerStatus();
+		//RpcId(id, nameof(SetServerInfo), serverStatus.Version, serverStatus.NextResetTs);
+	}
+
+	[Rpc(MultiplayerApi.RpcMode.Authority, CallLocal = false, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
+	public void SetServerInfo(string version, string nextReset)
+	{
+		throw new NotSupportedException();
+	}
+}
