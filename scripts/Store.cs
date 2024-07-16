f55d229cfd95fbbcd509107eb7c029cd90a7dfcd
stnokott <30934296+stnokott@users.noreply.github.com>
2024-07-16 23:19:32 +0200

    chore: avoid redundant (un)marshalling in store

--- scripts/Store.cs
+++ scripts/Store.cs
@@ -7,7 +7,7 @@ public partial class Store : Node
 {
 	public static Store Instance { get; private set; }
 
-	// TODO: check if we actually need a copy of each of these on the SERVER, otherwise we can stop calling the RPC methods on the server too (CallLocal=false)
+	// TODO: check if we actually need a copy of each of these on the SERVER, otherwise we can stop assigning values in the setters
 	private ServerStatusResource ServerInfo = new();
 	private AgentInfoResource AgentInfo = new();
 	private Array<ShipResource> Ships { get; set; } = new Array<ShipResource>();
@@ -22,10 +22,11 @@ public partial class Store : Node
 
 	public void SetServerStatus(ServerStatusResource serverInfo)
 	{
+		ServerInfo = serverInfo;
 		Rpc(nameof(RpcSetServerStatus), serverInfo.ToBytes());
 	}
 
-	[Rpc(MultiplayerApi.RpcMode.Authority, CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
+	[Rpc(MultiplayerApi.RpcMode.Authority, CallLocal = false, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
 	private void RpcSetServerStatus(byte[] data)
 	{
 		var unpacked = ServerStatusResource.FromBytes(data);
@@ -39,10 +40,11 @@ public partial class Store : Node
 
 	public void SetAgentInfo(AgentInfoResource agent)
 	{
+		AgentInfo = agent;
 		Rpc(nameof(RpcSetAgentInfo), agent.ToBytes());
 	}
 
-	[Rpc(MultiplayerApi.RpcMode.Authority, CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
+	[Rpc(MultiplayerApi.RpcMode.Authority, CallLocal = false, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
 	private void RpcSetAgentInfo(byte[] data)
 	{
 		var unpacked = AgentInfoResource.FromBytes(data);
@@ -67,11 +69,12 @@ public partial class Store : Node
 
 	public void AddShip(ShipResource ship)
 	{
+		Ships.Add(ship);
 		Rpc(nameof(RpcAddShip), ship.ToBytes());
 	}
 
 
-	[Rpc(MultiplayerApi.RpcMode.Authority, CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
+	[Rpc(MultiplayerApi.RpcMode.Authority, CallLocal = false, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
 	private void RpcAddShip(byte[] data)
 	{
 		Ships.Add(ShipResource.FromBytes(data));
